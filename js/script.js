6fe231b6eda577e21b8720852584cea4ea16ae7c
Trips722 <104538718+Trips722@users.noreply.github.com>
2022-05-20 22:08:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-/*
 
-  Author: Mihovil Ilakovac (infomiho)
-  Used: javascript + underscore.js (for templating), Foundation 5 (design)
-  
-  It's based on a simple idea, to minimize requests to the server and only send the final cart to the server for evaluation and payment.
-
-*/
 var cartId = "cart";
 
 var localAdapter = {
@@ -104,7 +97,7 @@ var helpers = {
     },
     updateTotal: function () {
 
-        this.setHtml('totalPrice', cart.total + '$');
+        this.setHtml('totalPrice', cart.total + ' Rs');
 
     }
 
